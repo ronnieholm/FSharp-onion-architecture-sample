9be9484783a896ea0e1a593f4174e6e6cd7f15c4
Ronnie Holm <mail@bugfree.dk>
2023-09-28 18:33:56 +0200

    Update

--- src/Scrum.Tests/IntegrationTests.fs
+++ src/Scrum.Tests/IntegrationTests.fs
@@ -34,26 +34,30 @@ type DisableParallelization() =
     class
     end
 
+// TODO: Do we really require disable parallelization? With SQLite, only one tx can be active at once anyway.
+// Even if we could run multiple tx in parallel, changes from one don't bleed into another. The clean-up of
+// data, to reset baseline, is why we run aren't running in parallel.
 [<Collection(nameof DisableParallelization)>]
 type StoryAggregateRequestTests() =
     let connectionString = "URI=file:/home/rh/Downloads/scrumfs.sqlite"
 
     let missing () = Guid.NewGuid()
 
-    let setupWith (env: IAppEnv) =
+    let setup (env: IAppEnv) =
         let r = env.StoryRepository
         let s = env.SystemClock
         let l = env.Logger
-        let ct = CancellationToken.None
-        CreateStoryCommand.runAsync r s l ct,
-        AddTaskToStoryCommand.runAsync r s l ct,
-        GetStoryByIdQuery.runAsync r l ct,
-        DeleteStoryCommand.runAsync r l ct,
-        DeleteTaskCommand.runAsync r l ct,
-        UpdateStoryCommand.runAsync r s l ct,
-        UpdateTaskCommand.runAsync r s l ct,
-        fun _ -> env.CommitAsync ct
-
+        let ct = CancellationToken.None        
+        {| CreateStory = CreateStoryCommand.runAsync r s l ct
+           AddTaskToStory = AddTaskToStoryCommand.runAsync r s l ct
+           GetStoryById = GetStoryByIdQuery.runAsync r l ct
+           DeleteStory = DeleteStoryCommand.runAsync r l ct
+           DeleteTask = DeleteTaskCommand.runAsync r l ct
+           UpdateStory = UpdateStoryCommand.runAsync r s l ct
+           UpdateTask = UpdateTaskCommand.runAsync r s l ct
+           Commit = fun _ -> env.CommitAsync ct
+           Rollback = fun _ -> env.RollbackAsync ct |}  
+    
     // TODO: move function out of any specific test as it clean all tables
     let resetDatabase () =
         // Run SQL statements in reverse dependency order.
@@ -64,204 +68,202 @@ type StoryAggregateRequestTests() =
         sql
         |> Array.iter (fun sql ->
             use cmd = new SQLiteCommand(sql, connection, transaction)
+            // TODO: use fold to accumulate records affected for debugging
             cmd.ExecuteNonQuery() |> ignore)
         transaction.Commit()
-
-    [<Fact>]
+        
     let ``create story with task`` () =
         use env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, getStory, _, _, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! result = createStory cmd
+            let! result = f.CreateStory cmd
             test <@ result = Ok(cmd.Id) @>
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
-            let! result = addTaskToStory cmd
+            let! result = f.AddTaskToStory cmd
             test <@ result = Ok(cmd.TaskId) @>
-            let! result = getStory { Id = cmd.StoryId }
+            let! result = f.GetStoryById { Id = cmd.StoryId }
             //test <@ result = Ok(_) @>
-            test <@ true @>
-            do! commit ()
-        }
+            do! f.Commit ()
+        }        
 
     [<Fact>]
     let ``create duplicate story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, _, _, _, _, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
-            let! result = createStory cmd
+            let! _ = f.CreateStory cmd
+            let! result = f.CreateStory cmd
             test <@ result = Error(CreateStoryCommand.DuplicateStory(cmd.Id)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``delete story without tasks`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, _, getStoryById, deleteStory, _, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
-            let! result = deleteStory { Id = cmd.Id }
+            let! _ = f.CreateStory cmd
+            let! result = f.DeleteStory { Id = cmd.Id }
             test <@ result = Ok(cmd.Id) @>
-            let! result = getStoryById { Id = cmd.Id }
+            let! result = f.GetStoryById { Id = cmd.Id }
             test <@ result = Error(GetStoryByIdQuery.StoryNotFound(cmd.Id)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``delete story with task`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, getStoryById, deleteStory, _, _, _, commit =
-                env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
-            let! _ = addTaskToStory cmd
-            let! result = deleteStory { Id = cmd.StoryId }
+            let! _ = f.AddTaskToStory cmd
+            let! result = f.DeleteStory { Id = cmd.StoryId }
             test <@ result = Ok(cmd.StoryId) @>
-            let! result = getStoryById { Id = cmd.StoryId }
+            let! result = f.GetStoryById { Id = cmd.StoryId }
             test <@ result = Error(GetStoryByIdQuery.StoryNotFound(cmd.StoryId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``add duplicate task to story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, _, _, _, _, _, commit = env |> setupWith
+            let f = env |> setup
             let createStoryCmd = A.createStoryCommand ()
             let addTaskCmd = { A.addTaskToStoryCommand () with StoryId = createStoryCmd.Id }
-            let! _ = createStory createStoryCmd
-            let! _ = addTaskToStory addTaskCmd
-            let! result = addTaskToStory addTaskCmd
+            let! _ = f.CreateStory createStoryCmd
+            let! _ = f.AddTaskToStory addTaskCmd
+            let! result = f.AddTaskToStory addTaskCmd
             test <@ result = Error(AddTaskToStoryCommand.DuplicateTask(addTaskCmd.TaskId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``add task to non-existing story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let _, addTaskToStory, _, _, _, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = { A.addTaskToStoryCommand () with StoryId = missing () }
-            let! result = addTaskToStory cmd
+            let! result = f.AddTaskToStory cmd
             test <@ result = Error(AddTaskToStoryCommand.StoryNotFound(cmd.StoryId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``delete existing task on story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, _, _, deleteTask, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
-            let! _ = addTaskToStory cmd
+            let! _ = f.AddTaskToStory cmd
             let cmd = { StoryId = cmd.StoryId; TaskId = cmd.TaskId }
-            let! result = deleteTask cmd
+            let! result = f.DeleteTask cmd
             test <@ result = Ok(cmd.TaskId) @>
-            do! commit ()
+            do! f.Commit ()
         }
 
     [<Fact>]
     let ``delete task on non-existing story`` () =
         let env = new AppEnv(connectionString)        
         task {
-            let createStory, _, _, _, deleteTask, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
             let cmd = { StoryId = missing (); TaskId = cmd.TaskId }
-            let! result = deleteTask cmd
+            let! result = f.DeleteTask cmd
             test <@ result = Error(DeleteTaskCommand.StoryNotFound(cmd.StoryId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``delete non-existing task on story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, _, _, _, deleteTask, _, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { StoryId = cmd.Id; TaskId = missing () }
-            let! result = deleteTask cmd
+            let! result = f.DeleteTask cmd
             test <@ result = Error(DeleteTaskCommand.TaskNotFound(cmd.TaskId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``update existing story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, _, _, _, _, updateStory, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = A.updateStoryCommand cmd
-            let! result = updateStory cmd
+            let! result = f.UpdateStory cmd
             test <@ result = Ok(cmd.Id) @>
-            do! commit ()
+            do! f.Commit ()
         }
 
     [<Fact>]
     let ``update non-existing story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let _, _, _, _, _, updateStory, _, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
             let cmd = A.updateStoryCommand cmd
-            let! result = updateStory cmd
+            let! result = f.UpdateStory cmd
             test <@ result = Error(UpdateStoryCommand.StoryNotFound(cmd.Id)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``update existing task`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, _, _, _, _, updateTask, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
-            let! _ = addTaskToStory cmd
+            let! _ = f.AddTaskToStory cmd
             let cmd = A.updateTaskCommand cmd
-            let! result = updateTask cmd
+            let! result = f.UpdateTask cmd
             test <@ result = Ok(cmd.TaskId) @>
-            do! commit ()
+            do! f.Commit ()
         }
 
     [<Fact>]
     let ``update non-existing task on existing story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, _, _, _, _, updateTask, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
-            let! _ = addTaskToStory cmd
+            let! _ = f.AddTaskToStory cmd
             let cmd = { A.updateTaskCommand cmd with TaskId = missing () }
-            let! result = updateTask cmd
+            let! result = f.UpdateTask cmd
             test <@ result = Error(UpdateTaskCommand.TaskNotFound(cmd.TaskId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     [<Fact>]
     let ``update task on non-existing story`` () =
         let env = new AppEnv(connectionString)
         task {
-            let createStory, addTaskToStory, _, _, _, _, updateTask, commit = env |> setupWith
+            let f = env |> setup
             let cmd = A.createStoryCommand ()
-            let! _ = createStory cmd
+            let! _ = f.CreateStory cmd
             let cmd = { A.addTaskToStoryCommand () with StoryId = cmd.Id }
-            let! _ = addTaskToStory cmd
+            let! _ = f.AddTaskToStory cmd
             let cmd = { A.updateTaskCommand cmd with StoryId = missing () }
-            let! result = updateTask cmd
+            let! result = f.UpdateTask cmd
             test <@ result = Error(UpdateTaskCommand.StoryNotFound(cmd.StoryId)) @>
-            do! commit ()
+            do! f.Rollback()
         }
 
     interface IDisposable with
